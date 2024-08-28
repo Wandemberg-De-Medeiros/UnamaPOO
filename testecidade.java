eafce7173c802ab47061bfffd8994cc70f6acbf0
Wandemberg-De-Medeiros <wandembergdemedeiros@gmail.com>
2024-08-28 09:34:52 -0300

    Update testecidade.java

--- testecidade.java
+++ testecidade.java
@@ -6,10 +6,28 @@ class testecidade {
     int idadeAnoQueVem = idade + 1;
     int resultado = idade % 2;
     double MetadeIdade = idade / 2;
+
+    boolean maiorIdade;
+    maiorIdade = idade >= 18;
+
+    boolean tipoSangue;
+    tipoSangue = 'a' == 'A';
+
+    int i = 5;
+    int j = 1;
+    i = i + 1; 
+
+    //if (idade >= 18)
+    //println("Deixar entrar")
+    //else:
     
     System.out.println(idade);
     System.out.println(idadeAnoQueVem);
     System.out.println(MetadeIdade);
     System.out.println(resultado);
+    System.out.println(maiorIdade);
+    System.out.println(tipoSangue);
+    System.out.println(i);
+    System.out.println(j);
    }
 }
