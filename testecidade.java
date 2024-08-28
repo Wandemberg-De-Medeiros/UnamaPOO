28a1e3e31ff3834240181e6857e4b06ad9a32eb2
Wandemberg-De-Medeiros <wandembergdemedeiros@gmail.com>
2024-08-28 11:57:06 +0000

    Aula02

---
+++ testecidade.java
@@ -0,0 +1,15 @@
+class testecidade {
+   public static void main(String[] args){
+    int idade;
+    idade = 15;
+    
+    int idadeAnoQueVem = idade + 1;
+    int resultado = idade % 2;
+    double MetadeIdade = idade / 2;
+    
+    System.out.println(idade);
+    System.out.println(idadeAnoQueVem);
+    System.out.println(MetadeIdade);
+    System.out.println(resultado);
+   }
+}
